ccb381b33ef360bbdea971a8a9575426e19a1b24
martian58 <fuadelizade6@gmail.com>
2025-05-25 10:52:38 +0400

    Update admin site

--- core/admin.py
+++ core/admin.py
@@ -5,13 +5,28 @@ from .models import Book, Author, Category, ContactMessage
 from parler.admin import TranslatableAdmin
 
 class BookAdmin(TranslatableAdmin):
-    list_display = ('title', 'price')
+    search_fields = ('translations__title', 'translations__description', 'isbn_number')
+    list_display = ('title','isbn_number','price')
 
 class CategoryAdmin(TranslatableAdmin):
     list_display = ('name',)
     search_fields = ('translations__name',)
 
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')   # columns in the admin list view
+    search_fields = ('name', 'email', 'message')     # search bar for these fields
+    list_filter = ('created_at',)                                # filter by date
+    ordering = ('-created_at',)                                  # default ordering
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname')                     # columns in the admin list view
+    search_fields = ('name', 'surname')                    # searchable fields
+    ordering = ('name', 'surname')                         # default ordering (alphabetical)
+
+
+
 admin.site.register(Book, BookAdmin)
-admin.site.register(Author)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(ContactMessage)
+admin.site.register(ContactMessage, ContactMessageAdmin)
+admin.site.register(Author, AuthorAdmin)
